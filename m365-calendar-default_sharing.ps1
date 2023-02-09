fc41539df702da241e1f8e24f5d6e4c5131c6cca
Emil Møller Lind <lind@noque.dk>
2023-02-09 19:03:29 +0100

    some

--- m365-calendar-default_sharing.ps1
+++ m365-calendar-default_sharing.ps1
@@ -1,12 +1,16 @@
 Import-module ExchangeOnlineManagement
 Connect-ExchangeOnline
 
+# Definer hvilken slags permission der skal sættes
+$DefaultCalendarPermissionToSet = "Reviewer"
+
+# Loop gennem alle bruger opjekter
 foreach($user in Get-Mailbox -RecipientTypeDetails UserMailbox) {
 
 $UPN = $user.UserPrincipalName
 $CalFolder = Get-MailboxFolderStatistics $UPN -FolderScope Calendar | select-object -First 1
 $Kalender = $UPN + ":\" + $calfolder.Name
-Write-Output $Kalender
-#Set-MailboxFolderPermission -Identity $cal -User Default -AccessRights LimitedDetails
-
+Write-Output "Setting permissions for " $UPN
+Set-MailboxFolderPermission -Identity $Kalender -User Default -AccessRights $DefaultCalendarPermissionToSet -WarningAction:SilentlyContinue
+Set-MailboxRegionalConfiguration -Identity $UPN -Language da-dk -DateFormat "dd-MM-yyyy" -LocalizeDefaultFolderName
 }
