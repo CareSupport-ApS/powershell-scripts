cf58efc3abaf3289f8f640f0b79d9ef875e8a3a1
Emil Møller Lind <lind@noque.dk>
2023-07-12 19:50:44 +0200

    some

--- archive-mailbox.ps1
+++ archive-mailbox.ps1
@@ -15,17 +15,28 @@ $AutoReplyTemplate = Get-Content -Path $AutoReplyFile -Raw
 
 do {
 $ArchiveUser = Read-Host -Prompt 'Enter e-mail of the user you want to archive'
+$PrivateContact = Read-Host -Prompt 'Enter the private contact information of the user, if none just press enter'
 $User = Get-Mailbox -Identity $ArchiveUser
 
 Write-Host "Will archive" $User.DisplayName
+
+$AutoReplyMessage = $AutoReplyTemplate -replace '\{NAME\}', $User.DisplayName
+if ($PrivateContact -ne "") {
+    $AutoReplyMessage = $AutoReplyTemplate -replace '\{PRIVATECONTACT_DK}', ($User.DisplayName + " kan kontaktes på: " + $PrivateContact)
+}
+else {
+    $AutoReplyMessage = $AutoReplyTemplate -replace '\{PRIVATECONTACT_DK}', ""
+}
+
+Write-Host $AutoReplyMessage
+
 Set-Mailbox -Identity $ArchiveUser -Type Shared
 
 Set-Mailbox -Identity $ArchiveUser -HiddenFromAddressListsEnabled $true
 
-$AutoReplyMessage = $AutoReplyTemplate -replace '\{NAME\}', $User.DisplayName
 
-$DisplayName = "[ARCHIVE] - " + $User.DisplayName.ToString()
-Set-User -Identity $ArchiveUser -DisplayName $DisplayName
-Set-MailboxAutoReplyConfiguration -Identity $ArchiveUser -ExternalAudience All -AutoReplyState Enabled -ExternalMessage $AutoReplyMessage -InternalMessage $AutoReplyMessage
+#$DisplayName = "[ARCHIVE] - " + $User.DisplayName.ToString()
+#Set-User -Identity $ArchiveUser -DisplayName $DisplayName
+#Set-MailboxAutoReplyConfiguration -Identity $ArchiveUser -ExternalAudience All -AutoReplyState Enabled -ExternalMessage $AutoReplyMessage -InternalMessage $AutoReplyMessage
 $Continue = Read-Host -Prompt 'Do you want to archive another user? (Y/N)'
 } while ($Continue -eq 'Y')
