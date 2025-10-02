979e5959958d4e38a7a5710c045b4f2e5e76b299
Emil Møller Lind <lind@noque.dk>
2025-10-02 09:44:56 +0200

    update script

--- archive-mailbox.ps1
+++ archive-mailbox.ps1
@@ -27,8 +27,6 @@ $ArchiveUser = Read-Host -Prompt 'Enter e-mail of the user you want to archive'
 $PrivateContact = Read-Host -Prompt 'Enter the private contact information of the user, if none just press enter'
 $User = Get-Mailbox -Identity $ArchiveUser
 
-Write-Host "Will archive" $User.DisplayName
-
 $AutoReplyMessage = $AutoReplyTemplate -replace '\{NAME\}', $User.DisplayName
 
 if ($PrivateContact -ne "") {
@@ -42,13 +40,32 @@ else {
 
 Write-Host $AutoReplyMessage
 
+Write-Host "Will convert user to Shared Mailbox: " $User.DisplayName
 Set-Mailbox -Identity $ArchiveUser -Type Shared
-Set-MailboxAutoReplyConfiguration -Identity $ArchiveUser -ExternalAudience All -AutoReplyState Enabled -ExternalMessage $AutoReplyMessage -InternalMessage $AutoReplyMessage
+
+$CurrentConfig = Get-MailboxAutoReplyConfiguration -Identity $ArchiveUser
+if ($CurrentConfig.AutoReplyState -eq "Enabled") {
+ Write-Host "User $ArchiveUser already has an autoreply configured:"
+$Override = Read-Host -Prompt 'Do you want to override the existing autoreply? (Y/N)'
+if ($Override -eq 'Y') {
+            Set-MailboxAutoReplyConfiguration -Identity $ArchiveUser -ExternalAudience All -AutoReplyState Enabled -ExternalMessage $AutoReplyMessage -InternalMessage $AutoReplyMessage
+            Write-Host "Autoreply updated."
+        } else {
+            Write-Host "No changes applied."
+        }
+} else {
+     # No active autoreply, safe to apply
+        Set-MailboxAutoReplyConfiguration -Identity $ArchiveUser -ExternalAudience All -AutoReplyState Enabled -ExternalMessage $AutoReplyMessage -InternalMessage $AutoReplyMessage
+        Write-Host "Autoreply applied."
+}
+
 
 Set-Mailbox -Identity $ArchiveUser -HiddenFromAddressListsEnabled $true
+Write-Host "User hidden from AddressList"
 
 $DisplayName = "[ARCHIVE] - " + $User.DisplayName.ToString()
 Set-User -Identity $ArchiveUser -DisplayName $DisplayName
+Write-Host "Display name changed to " $User.DisplayName
 
 $Continue = Read-Host -Prompt 'Do you want to archive another user? (Y/N)'
 } while ($Continue -eq 'Y')
